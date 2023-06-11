540b5b7219ae906657c58b005ed51fcc5f62337f
Michael Yocca <mjyocca@gmail.com>
2023-06-11 10:15:11 -0700

    add fiber, gorm and auth package

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/mjyocca/go-auth/backend/pkg/auth"
+)
+
+func newFiber() *fiber.App {
+	app := fiber.New()
+	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+		AllowMethods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+	}))
+	return app
+}
 
 func main() {
-	fmt.Println("Hello World")
+
+	app := newFiber()
+
+	auth.RegisterRoutes(app)
+
+	if err := app.Listen(":8000"); err != nil {
+		fmt.Printf("%v", err)
+	}
 }
